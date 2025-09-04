6e4e338f550df226f7a51337e4a7e0a255ccb1ed
CarlDots <110611820+carldots@users.noreply.github.com>
2025-09-04 15:51:46 +0200

    2025-09-04 tempus before writeCache refactor

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-# my_list = ["what", "test", "conf"]
-my_list = []
+# saved_list = ["what", "test", "conf"]
+cached_list = []
+local_changes = []
+
+#INFO: 2025-09-04 stopped at realizing my local_chages thing breaks due to the fact of removing items
 
 with open("cache", "r") as cache:
     cache = cache.readlines()
     for line in cache:
         print(line, sep="", end="")
         #INFO: No idea what more it strips
-        my_list.append(line.strip())
+        cached_list.append(line.strip())
+
+def writeCache(*args):
+    with open("cache", "a") as cache:
+        for arg in args:
+            print(arg, sep="", end="")
+            cache.write(f"{arg}\n")
 
 user_input = input("""
 1. Add item
@@ -16,54 +25,42 @@ user_input = input("""
 5. Quit
 """)
 
-#FIX: make functions for repititions
-
 def menu():
     if user_input == "1":
-        my_list.append(input("type item to add"))
-        print("item added")
+        ask_add_item()
     elif user_input == "2":
-        print(my_list)
+        print(cached_list)
     elif user_input == "3":
-        for i, elem in enumerate(my_list):
-            print(f"{i} {elem}")
-        index = int(input("please input index to update"))
-        my_list[index] = input("input new value") 
-        print(my_list)
+        ask_update_item()
     elif user_input == "4":
-        for i, elem in enumerate(my_list):
-            print(f"{i} {elem}")
-        index = int(input("please input index to remove"))
-        del my_list[index]
-        print(my_list)
+        ask_remove()
     elif user_input == "5":
         return
 
 def ask_add_item():
-    my_list.append(input("type item to add"))
+    local_changes.append(input("type item to add"))
+    writeCache(input("type item to add"))
     print("item added")
 
-def show_all():
-    print(my_list)
-
 def ask_update_item():
-    for i, elem in enumerate(my_list):
+    for i, elem in enumerate(cached_list):
         print(f"{i} {elem}")
     index = int(input("please input index to update"))
-    my_list[index] = input("input new value") 
-    print(my_list)
+    cached_list[index] = input("input new value") 
+    print(cached_list)
 
 def ask_remove():
-    for i, elem in enumerate(my_list):
+    for i, elem in enumerate(cached_list):
         print(f"{i} {elem}")
     index = int(input("please input index to remove"))
-    del my_list[index]
-    print(my_list)
+    del cached_list[index]
+    print(cached_list)
 
 #FIX: Implement error handling
 
 menu()
 
-with open("cache", "a") as f:
-    for item in my_list:
-        f.write(item)
+#FIX: just duplicates everything
+# with open("cache", "a") as f:
+#     for item in cached_list:
+#         f.write(f"{item}\n")
